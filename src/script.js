0468a059f1da7aa847d8fad09980e487a94405db
Michael Hancock <shanehancock64@live.com>
2022-05-11 11:55:19 -0700

    contact

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {navBar()};
 
 // Get the header
 var header = document.querySelector('header');
@@ -8,7 +8,7 @@ var header = document.querySelector('header');
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function navBar() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
